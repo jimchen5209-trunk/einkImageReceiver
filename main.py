3162b2c16a71cfdf37119c91e5904804da04e47c
jimchen5209 <jimchen5209@gmail.com>
2022-11-09 15:09:26 +0800

    feat: main function

--- main.py
+++ main.py
@@ -1,1 +1,50 @@
-# main.py -- put your code here!
+import json
+from waveshare_epd import epd1in54_V2
+from mqtt import MQTT
+
+class EInkReceiver():
+    def __init__(self):
+        with open('./config.json', 'r') as f:
+            self.data = json.loads(f.read())
+
+        self.__error_logger = ErrorLogger()
+        errors = self.__error_logger.read_error()
+        if len(errors) != 0:
+            print(errors)
+            self.__error_logger.clear_error()
+        self.epd = epd1in54_V2.EPD()
+        self.epd.init(0)
+        self.epd.Clear(0xFF)
+
+        self.__display_init()
+        self.mqtt = MQTT()
+        self.mqtt.set_on_message(self.on_message)
+
+    def __display_init(self):
+        with open('init.txt', 'r') as fs:
+            self.epd.display(eval(fs.readline()))
+    
+    def __display_ready(self):
+        with open('ready.txt', 'r') as fs:
+            self.epd.display(eval(fs.readline()))
+
+    def on_message(self, topic, message):
+        print(topic.decode('UTF-8'))
+        if topic.decode('UTF-8') == self.data['mqtt']['topic']:
+            if message.decode('UTF-8') != "Error":
+                self.epd.init(0)
+                try:
+                    data = eval(message)
+                    self.epd.display(data)
+                except Exception as e:
+                    print(e)
+                    self.mqtt.send_message("Error")
+                self.epd.sleep()
+
+    def listen(self):
+        self.__display_ready()
+        self.epd.sleep()
+        self.mqtt.listen()
+
+main = EInkReceiver()
+main.listen()
