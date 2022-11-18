129273ce492d98877d46a3635618acf6fe3cde67
jimchen5209 <jimchen5209@gmail.com>
2022-11-18 13:18:40 +0800

    fix: Pin should be out

--- led.py
+++ led.py
@@ -5,13 +5,12 @@ class Led:
     def __init__(self):
         with open('./config.json', 'r') as f:
             self.__data = json.loads(f.read())
-        self.__led = Pin(self.__data['pins']['indicator_led']['pin'])
+        self.__led = Pin(self.__data['pins']['indicator_led']['pin'], Pin.OUT)
         self.__led_on = 0 if self.__data['pins']['indicator_led']['invert'] else 1
         self.__led_off = 1 if self.__data['pins']['indicator_led']['invert'] else 0
-    
+
     def turn_on(self):
         self.__led(self.__led_on)
 
     def turn_off(self):
         self.__led(self.__led_off)
-
