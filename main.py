2b3ed825521ea353de9f8b2ac132afddad120590
jimchen5209 <jimchen5209@gmail.com>
2022-11-18 17:07:29 +0800

    feat: add 7.5 inch black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import gc
 import json
-from waveshare_epd import epd7in5bc
+from waveshare_epd import epd7in5
 from errorlog import ErrorLogger
 from mqtt import MQTT
 import framebuf
@@ -16,7 +16,7 @@ class EInkReceiver():
 
         self.__error_logger = ErrorLogger()
         errors = self.__error_logger.read_error()
-        self.epd = epd7in5bc.EPD()
+        self.epd = epd7in5.EPD()
         self.epd.init()
         if len(errors) != 0:
             print("Recovered from error")
@@ -41,11 +41,6 @@ class EInkReceiver():
                 if message == "CLEAR":
                     self.epd.init()
                     self.epd.Clear()
-                elif message == "BLANK":
-                    buf = bytearray(self.epd.width * self.epd.height// 8)
-                    fb = framebuf.FrameBuffer(buf, self.epd.width, self.epd.height, framebuf.MONO_HLSB)
-                    fb.fill(1)
-                    self.__save(topic, buf)
                 elif "ERROR" not in message:
                     self.__save(topic, eval(message))
 
@@ -54,17 +49,7 @@ class EInkReceiver():
     def __save(self, topic, buffer):
         if topic.decode('UTF-8').endswith("black"):
             self.__temp_black = buffer
-            if self.__temp_red != None:
-                self.__display()
-            else:
-                print("Black stored, waiting for red.")
-
-        if topic.decode('UTF-8').endswith("red"):
-            self.__temp_red = buffer
-            if self.__temp_black != None:
-                self.__display()
-            else:
-                print("Red stored, waiting for black.")
+            self.__display()
 
     def __display(self):
         self.epd.init()
@@ -75,7 +60,6 @@ class EInkReceiver():
             print(e)
             self.mqtt.send_message("ERROR")
         self.__temp_black = None
-        self.__temp_red = None
         self.epd.sleep()
         gc.collect()
 
